73e69548cebc7c87b3000a0b0cb58db5960f6cbd
jfrogsolutionci <jfrog-solutions-ci@jfrog.com>
2019-02-13 15:38:01 -0800

    initial

---
+++ holiday.go
@@ -0,0 +1,12 @@
+package social
+
+import (
+    "fmt"
+    "github.com/jfrog-solutiontest/employee"
+)
+
+func presidentPackageName () {
+	fmt.Println ("Package Name: president")
+        employee.sdet()
+}
+
